0e842e31d6646ffedb81e65a3aef5a3d67d6f74f
Kuldeep Rathore <kuldeeprathore9440@gmail.com>
2025-11-14 15:43:27 +0530

    testing

---
+++ class7.py
@@ -0,0 +1,199 @@
+#for i in range(1,5,1):
+#  print(i)
+
+  
+#a=range(1,10,1) 
+#for i in a:
+#    print("hello how are you")
+
+
+#for i in range(10,41):
+#   print(i)
+    
+
+#for i in range(-10,21):
+#    print(i)
+
+
+#for i in range(35,4,-1):
+#    print(i)
+ 
+
+#for i in range(5,51,5):
+#    print(i)
+    
+#n=int(input("enter a number:-"))
+#for i in range(n,(n*10)+1,n):
+#    print(i)
+
+
+#a="Hello how are you"
+
+#for i in a: #first way
+#    print(i)
+    
+#for i in range(0,len(a),1):
+#    print(a[i])  
+
+
+#for i in range(1,101,25):
+#    print(i)
+
+
+
+#for i in range(1,11,1):
+#    print(f" {i} - hello how are you")
+
+#for i in range(10,41,1):
+#    print(i) 
+
+#for i in range(-10,21,1):
+#    print(i)
+
+#for i in range(34,4,-1):
+#    print(i)
+
+#for i in range(5,51,5):
+#    print(i)
+
+#t = int(input("enter a number:="))
+#for i in range(t,(t*10)+1,t):
+#    print(i)
+
+#s="Hello how are you"
+
+#for i in s:
+#    print(i)
+
+#for i in range(len(s)):
+#    print(s[i])
+
+#n="NATURE"
+#for i in range(0,len(n),1):
+#    print(n[i])
+
+
+#q=int(input("enter a number-"))
+#for i in range(q):
+#    print("Hello World")
+
+
+
+
+#x=int(input("enter a number-"))
+#for i in range(1,x+1,1):
+#    print(i)
+
+
+#z=int(input("enter a number-"))
+#for i in range(z,0,-1):
+#    print(i)
+
+
+"""n=int(input("enter a number:-"))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print(sum)  """  
+
+
+'''n=int(input("enter a numbe:-"))
+fact=1
+for i in range(n,1,-1):
+    fact*=i
+print(fact) '''   
+    
+    
+"""n=int(input("enter a number_")) 
+ev=0
+ov=0
+for i in range(1,n+1):
+    if i%2==0:
+        ev=ev+i
+    else:
+        ov=ov+i
+print(f"sum of odd id {ov}")
+print(f"sum of even is {ev}") """
+
+
+'''n=int(input("enter a number_"))
+
+for i in range(1,n+1):
+    if n%i==0:
+        print(i)
+    else:
+        pass'''
+        
+        
+        
+"""n=int(input("enter a number-")) 
+
+for i in range(2,n):
+    if n%i==0:
+        print("this is not a prime number")
+        break
+else:
+    print("this is a prime number")  """ 
+         
+        
+            
+'''n=int(input("enter a number_"))
+sum1 = 0
+for i in range(1,n+1):
+    if n%i==0:
+        sum1 = sum1+i
+
+print(sum1) '''       
+    
+"""n=int(input("tell your base_"))  
+m=int(input("tell your pow_"))  
+pow=n
+
+for i in range(1,m):
+    pow=pow*n
+    
+print(f"your power value is {pow}") """
+
+
+"""n=int(input("num-"))
+sum_fact=0
+for i in range(1,n+1):
+    if n%i==0:
+        sum_fact+=i
+print(sum_fact)  """
+
+
+"""base=int(input("num-"))
+pow=int(input("num-"))
+
+result=base
+for i in range(pow-1):
+    result*=base
+print(f" the result of power is {result}")   """ 
+    
+ 
+#PRIME NUMBER CHECKER 
+   
+"""n=int(input("num:-"))
+count=0
+for i in range(1,n+1):
+    if n%i==0:
+        count+=1
+if count==2:
+    print("this is a prime number number")
+else:
+    print("this is not a prime number") """
+    
+    
+n=int(input("number_"))
+
+for i in range(2,n):
+    if n%i==0:
+        print("not a prime number")
+        break
+else:
+    print("prime")               
+        
+
+   
+      
---
+++ class8.py
@@ -0,0 +1,33 @@
+#a=10
+
+'''while a>=0:
+    print(a)
+    a = a-1'''
+    
+    
+'''for i in range(1,11,1):
+    if i==6:
+        continue
+    print(i)'''
+
+    
+#a=1
+
+'''while a<=10:
+    print(a)
+    a=a+1
+    if a==3:
+        continue'''
+        
+        
+        
+        
+        
+sum1=0        
+for i in range(1,10000001):
+    a=i**2
+    sum1=sum1+a
+print(sum1)      
+        
+        
+        
